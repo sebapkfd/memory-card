4f21d8ebfa9206db311a9f96a749a9e4f39b4aa6
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-07-21 22:44:09 -0400

    Started to work in instructions

---
+++ src/components/Instructions.js
@@ -0,0 +1,11 @@
+const Instructions = () => { 
+    return (
+        <div>
+            <p>
+                This game consists in selecting all the items without repeating any of them.
+            </p>
+        </div>
+    )
+}
+
+export default Instructions
