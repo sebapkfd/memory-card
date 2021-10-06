050b9dbe7cae9512ab5ce49ebc81de1f4732f348
Sebastian Soto <sebastian.pkfd@gmail.com>
2021-10-06 19:39:54 -0300

    Removed unused component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import Game from "./components/Game";
+import React from "react";
+import Title from "./components/Title";
+import ScoreBoard from "./components/Scoreboard";
+import CardBoard from "./components/Cardboard";
+import Instructions from "./components/Instructions";
 import "./App.css";
 
 function App() {
     return (
         <div className="App">
-            <Game/>
+            <Title />
+            <Instructions />
+            <ScoreBoard />
+            <CardBoard/>
         </div>
     )
 }
